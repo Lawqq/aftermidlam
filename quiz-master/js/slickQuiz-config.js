2768a8bb364413c80b82b6562551c85aca0f698c
thomasgriffinx <xytomg@gmail.com>
2017-03-01 13:49:50 +0000

    push

---
+++ quiz-master/js/slickQuiz-config.js
@@ -0,0 +1,60 @@
+// Setup your quiz text and questions here
+
+// NOTE: pay attention to commas, IE struggles with those bad boys
+
+var quizJSON = {
+    "info": {
+        "name":    "Midlam Improvements - How do you think we did?",
+        "main":    "<p>Now that you have used the old site and our new one, How do you think we did?</p>",
+        "results": "<h5>Learn More</h5><p>Feedback text.</p>",
+        "level1":  "Competent Improvements",
+        "level2":  "The old design was better",
+        "level3":  "Excellent"
+    },
+    "questions": [
+        { 
+            "q": "The new designs best feature was",
+            "a": [
+                {"option": "midlam mock answer",     "correct": false},
+                {"option": "midlam mock answer",                 "correct": false},
+                {"option": "midlam mock answer",       "correct": true},
+                {"option": "midlam mock answer",          "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span>DRA = Digital Reference Architecture</p>",
+            "incorrect": "<p><span>Incorrect</span>" // no comma here
+        },
+        { 
+            "q": "The new designs best feature was",
+            "a": [
+                {"option": "midlam mock answer",     "correct": false},
+                {"option": "midlam mock answer",                 "correct": false},
+                {"option": "midlam mock answer",       "correct": true},
+                {"option": "midlam mock answer",          "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span>DRA = Digital Reference Architecture</p>",
+            "incorrect": "<p><span>Incorrect</span>" // no comma here
+        },
+           { 
+             "q": "The new designs best feature was",
+            "a": [
+                {"option": "midlam mock answer",     "correct": false},
+                {"option": "midlam mock answer",                 "correct": false},
+                {"option": "midlam mock answer",       "correct": true},
+                {"option": "midlam mock answer",          "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span>DRA = Digital Reference Architecture</p>",
+            "incorrect": "<p><span>Incorrect</span>" // no comma here
+        },
+         { 
+             "q": "The new designs best feature was",
+            "a": [
+                {"option": "midlam mock answer",     "correct": false},
+                {"option": "midlam mock answer",                 "correct": false},
+                {"option": "midlam mock answer",       "correct": true},
+                {"option": "midlam mock answer",          "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span>DRA = Digital Reference Architecture</p>",
+            "incorrect": "<p><span>Incorrect</span>" // no comma here
+        },
+    ]
+};
